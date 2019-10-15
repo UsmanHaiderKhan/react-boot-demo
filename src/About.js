4e5012d2e3aa3c0ee6e4974cd786ed30bb9b3573
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-10-15 12:06:39 +0500

    update added file for Routing links

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const About = () => (
+	<div>
+		<h2>About ME</h2>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora earum blanditiis delectus
+			esse eaque odio doloremque aspernatur, quos assumenda aut vitae quam repudiandae ut? Obcaecati
+			tempora ad voluptates eos illum.
+		</p>
+	</div>
+);
---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Contact = () => (
+	<div>
+		<h2>Contact ME</h2>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora earum blanditiis delectus
+			esse eaque odio doloremque aspernatur, quos assumenda aut vitae quam repudiandae ut? Obcaecati
+			tempora ad voluptates eos illum.
+		</p>
+	</div>
+);
