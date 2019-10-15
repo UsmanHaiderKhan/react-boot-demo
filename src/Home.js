33e842df9c2bf3635b05db90ef499f18ce346de7
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-10-15 12:05:11 +0500

    Update added different file for the sake of Routing

---
+++ src/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export const Home = () => (
+	<div>
+		<h2>Home</h2>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora earum blanditiis delectus
+			esse eaque odio doloremque aspernatur, quos assumenda aut vitae quam repudiandae ut? Obcaecati
+			tempora ad voluptates eos illum.
+		</p>
+		<h2>Home</h2>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora earum blanditiis delectus
+			esse eaque odio doloremque aspernatur, quos assumenda aut vitae quam repudiandae ut? Obcaecati
+			tempora ad voluptates eos illum.
+		</p>
+		<h2>Home</h2>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora earum blanditiis delectus
+			esse eaque odio doloremque aspernatur, quos assumenda aut vitae quam repudiandae ut? Obcaecati
+			tempora ad voluptates eos illum.
+		</p>
+		<h2>Home</h2>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora earum blanditiis delectus
+			esse eaque odio doloremque aspernatur, quos assumenda aut vitae quam repudiandae ut? Obcaecati
+			tempora ad voluptates eos illum.
+		</p>
+		<h2>Home</h2>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora earum blanditiis delectus
+			esse eaque odio doloremque aspernatur, quos assumenda aut vitae quam repudiandae ut? Obcaecati
+			tempora ad voluptates eos illum.
+		</p>
+		<h2>Home</h2>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora earum blanditiis delectus
+			esse eaque odio doloremque aspernatur, quos assumenda aut vitae quam repudiandae ut? Obcaecati
+			tempora ad voluptates eos illum.
+		</p>
+	</div>
+);
